f70bfb0e7a58c6280f7e06b13b10dd6ad43475ed
spanandequipment <ilya.i@skinact.com>
2024-11-15 15:04:37 -0800

    frontpage

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,20 @@
+import { Sidebar } from 'lucide-react'
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const Layout = (props: Props) => {
+    return (
+        <div className="flex overflow-hidden h-screen">
+            <Sidebar />
+            <div className="w-full">
+                {/* <InfoBar /> */}
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default Layout
